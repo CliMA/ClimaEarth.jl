525d4341438aa8f9f7ded1307b07849c4100299a
Navid C. Constantinou <navidcy@users.noreply.github.com>
2025-03-19 00:01:55 -0400

    updates

--- examples/aquaplanet/sphere_visualize_aquaplanet.jl
+++ examples/aquaplanet/sphere_visualize_aquaplanet.jl
@@ -22,8 +22,10 @@ function geographic2cartesian(λ, φ, r=1)
 end
 
 ζot = FieldTimeSeries(ocean_filename, "ζo")
+Tot = FieldTimeSeries(ocean_filename, "To")
 uat = FieldTimeSeries(atmos_filename, "ua")
 Tat = FieldTimeSeries(atmos_filename, "Ta")
+
 Nt = length(Tat)
 
 grid = uat.grid
@@ -35,35 +37,40 @@ n = Observable(1)
 ζon = @lift interior(ζot[$n], :, :, 1)
 uan = @lift interior(uat[$n], :, :, 1)
 Tan = @lift interior(Tat[$n], :, :, 1)
+Ton = @lift interior(Tot[$n], :, :, 1)
 
-fig = Figure(size=(1400, 700))
+fig = Figure(size=(1400, 1400))
 kw = (elevation=0.6, azimuth=5, aspect=:equal)
 
-axζo = Axis3(fig[1, 1]; kw...)
-axua = Axis3(fig[1, 2]; kw...)
-axTa = Axis3(fig[1, 3]; kw...)
-
-sf = surface!(axζo, x, y, z, color=ζon, colorrange=(-5e-5, 5e-5), colormap=:balance, nan_color=:lightgray)
-Colorbar(fig[2, 1], sf, width=Relative(0.6), vertical=false, label = "Ocean vorticity (s⁻¹)", labelsize=20)
+axua = Axis3(fig[1, 1]; kw...)
+axTa = Axis3(fig[1, 2]; kw...)
+axζo = Axis3(fig[3, 1]; kw...)
+axTo = Axis3(fig[3, 2]; kw...)
 
 sf = surface!(axua, x, y, z, color=uan, colorrange=(-40, 40), colormap=:balance, nan_color=:lightgray)
-Colorbar(fig[2, 2], sf, width=Relative(0.6), vertical=false, label = "Atmosphere zonal velocity (m s⁻¹)", labelsize=20)
+Colorbar(fig[2, 1], sf, width=Relative(0.6), vertical=false, label = "Atmosphere zonal velocity (m s⁻¹)", labelsize=20)
 
 sf = surface!(axTa, x, y, z, color=Tan, colorrange=(270, 310), colormap=:magma, nan_color=:lightgray)
-Colorbar(fig[2, 3], sf, width=Relative(0.6), vertical=false, label = "Atmosphere surface temperature (K)", labelsize=20)
+Colorbar(fig[2, 2], sf, width=Relative(0.6), vertical=false, label = "Atmosphere surface temperature (K)", labelsize=20)
+
+sf = surface!(axζo, x, y, z, color=ζon, colorrange=(-5e-5, 5e-5), colormap=:balance, nan_color=:lightgray)
+Colorbar(fig[4, 1], sf, width=Relative(0.6), vertical=false, label = "Ocean vorticity (s⁻¹)", labelsize=20)
+
+sf = surface!(axTo, x, y, z, color=Ton, colorrange=(270, 310), colormap=:magma, nan_color=:lightgray)
+Colorbar(fig[4, 2], sf, width=Relative(0.6), vertical=false, label = "Ocean SST (K)", labelsize=20)
 
 t = uat.times
-title = @lift @sprintf("%s days", round((t[$n]+1e-6) / Oceananigans.Units.day, digits=2))
+title = @lift @sprintf("%s days", round((t[$n] + 1e-6) / Oceananigans.Units.day, digits=2))
 Label(fig[0, :], title, fontsize=24)
 
-hidedecorations!(axζo)
-hidedecorations!(axua)
-hidedecorations!(axTa)
-hidespines!(axζo)
-hidespines!(axua)
-hidespines!(axTa)
+for ax in (axua, axTa, axζo, axTo)
+    hidedecorations!(ax)
+    hidespines!(ax)
+end
 
 rowgap!(fig.layout, 1, Relative(-0.1))
+rowgap!(fig.layout, 3, Relative(-0.1))
+
 colgap!(fig.layout, 1, Relative(-0.07))
 colgap!(fig.layout, 2, Relative(-0.07))
 
@@ -73,4 +80,3 @@ record(fig, "aquaplanet_sphere.mp4", 1:Nt, framerate = 16) do nn
     @info "Drawing frame $nn of $Nt..."
     n[] = nn
 end
-
